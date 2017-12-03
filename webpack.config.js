c576e798182a13b23a609eba423c9ab526783e70
cameronwp <cameron.w.pittman@gmail.com>
2017-12-03 17:40:57 -0600

    fix package name

--- webpack.config.js
+++ webpack.config.js
@@ -20,7 +20,11 @@ module.exports = {
     rules: [
       { test: /\.tsx?$/, use: 'awesome-typescript-loader' },
       { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-      { test: /\.scss$/, use: [{loader: 'style-loader'}, {loader: 'css-loader'}, {loader: 'sass-loader'}] }
+      { test: /\.scss$/, use: [
+        {loader: 'style-loader'},
+        {loader: 'css-loader'},
+        {loader: 'sass-loader'}
+      ] }
     ]
   }
 };
